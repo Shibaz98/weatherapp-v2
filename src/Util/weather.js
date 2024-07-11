e13cdd255ae58305ec1ba53b86da0ec293dbdc05
Shibaz98 <shibaz98@gmail.com>
2024-07-11 18:06:39 +0100

    getting a response from the API, now to plug it all into the app

--- src/Util/weather.js
+++ src/Util/weather.js
@@ -1,11 +1,12 @@
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 
-const key = ''; 
+const key = '4ea62206457808993127bcd02b7f17cd'; 
+
 
 const Weather = {
     async search(term){
-        return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${term}&appid=${key}`).then(response =>{
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${term}&appid=${key}`).then(response =>{
             if(response.ok){
                 return response.json();
             } else{
