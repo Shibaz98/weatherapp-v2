7ea87efcac7b6e0fc98746abd61aaff155d0054b
Shibaz98 <shibaz98@gmail.com>
2024-07-16 14:07:22 +0100

    successfully filtered through the fiveday forecast array and only returned 5 days

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [weather, setWeather] = useState('')
   const [fiveday, setFiveday] = useState([])
   const [test, setTest] = useState([]); 
+  const [forecast, setForecast] = useState([]); 
 
  
  const search = (term) =>{
@@ -20,11 +21,24 @@ function App() {
 
  const getForecast = (term) =>{
     Weather.getFiveday(term).then(setFiveday);
-
+    setForecast(fiveday.filter((object) => object.dt_txt.includes('12:00'))); // The api (fiveday) returns an array of 40 objects, this function allows me to filter through them all and ONLY return the ones that includes the time stamp 12:00pm
  };
 
-console.log(fiveday)
- 
+console.log(Array.isArray(fiveday))  // this is deffo an array lol 
+console.log(fiveday); 
+
+console.log(forecast[3].main.temp)
+
+
+  
+
+
+
+
+
+
+
+
  
   
  
